91b4297e52ce98a13e001d1486e3a7fee9497b68
Kur <deadkuriel@gmail.com>
2018-06-09 06:18:26 -0500

    Cambio de escena
    De Bioma 1 a Transicion 1

---
+++ Nahual/Assets/Scripts/Trans1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Trans1 : MonoBehaviour {
+
+	private bool bandera;
+
+	void Awake(){
+		bandera = true;
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (bandera) {
+			SceneManager.LoadScene ("Transicion1", LoadSceneMode.Single);
+			foreach (Transform child in GameObject.Find("Bioma1").transform) {
+				child.gameObject.SetActive (false);
+
+			}
+			bandera = false;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D col){
+		if (!bandera) {
+			bandera = true;
+		}
+	}
+}
